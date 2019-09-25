ba171c62ddd6f200ac79465997c19bf54a88552d
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-25 17:54:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,11 +7,11 @@ class Person
     @name = new_name
   end
   
-  def breed
-    @breed
+  def job
+    @job
   end
   
-  def breed=(new_breed)
-    @breed = new_breed
+  def job(new_job)
+    @job = new_job
   end
 end
